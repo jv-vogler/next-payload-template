517967676721ba274475f22037fe81349bc8e2fc
jv-vogler <jvsvogler@gmail.com>
2026-04-14 23:45:38 -0300

    feat: port pure core/ types from jv-portfolio (minus portfolio/skills)

---
+++ src/core/index.ts
@@ -0,0 +1,5 @@
+export * as Blog from "./blog";
+export * as Contact from "./contact";
+export * as Navigation from "./navigation";
+export * as Social from "./social";
+export * as Theme from "./theme";
